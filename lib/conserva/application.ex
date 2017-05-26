9c7ca128cf8429f096736db10939dedc7e176ed6
evanilukhin <evanilukhin@gmail.com>
2017-05-26 09:09:01 +0300

    Fixed order child in App Supervisor

--- lib/conserva/application.ex
+++ lib/conserva/application.ex
@@ -5,9 +5,9 @@ defmodule Conserva.Application do
   def start(_type, _args) do
     children = [
           supervisor(Conserva.Repo, []),
-          Plug.Adapters.Cowboy.child_spec(:http, Conserva.Router, [], [port: 4001]),
           worker(Conserva.ConvertersServer, [[name: ConvertersInfoServer]]),
-          supervisor(Conserva.ConvertersSupervisor, [[]])
+          supervisor(Conserva.ConvertersSupervisor, [[]]),
+          Plug.Adapters.Cowboy.child_spec(:http, Conserva.Router, [], [port: 4001])
     ]
     opts = [strategy: :one_for_one, name: Conserva.Application]
     Supervisor.start_link(children, opts)
